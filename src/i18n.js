89e5e99131be36c880cc630a2c913da5e2acbf6a
Valery <valefourin@gmail.com>
2024-03-10 17:13:54 +0200

    change fallback language to en

--- src/i18n.js
+++ src/i18n.js
@@ -3,8 +3,8 @@ import Backend from 'i18next-xhr-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
-const fallbackLng = ['es']; 
-const availableLanguages = ['es', 'en'];
+const fallbackLng = ['en']; 
+const availableLanguages = ['en', 'es2q'];
 
 i18n
   .use(Backend) // load translation using xhr -> see /public/locales. We will add locales in the next step
@@ -27,38 +27,3 @@ i18n
   });
 
 export default i18n;
-
-
-
-// import i18n from "i18next";
-// import detector from "i18next-browser-languagedetector";
-// import { initReactI18next } from "react-i18next";
-
-// import translationEN from './locales/en/translation.json';
-// import translationRU from './locales/ru/translation.json';
-
-// // the translations
-// const resources = {
-//   en: {
-//     translation: translationEN
-//   },
-//   ru: {
-//     translation: translationRU
-//   }
-// };
-
-// i18n
-//   .use(detector)
-//   .use(initReactI18next) // passes i18n down to react-i18next
-//   .init({
-//     resources,
-//     fallbackLng: "ru", // use ru if detected lng is not available
-
-//     keySeparator: false, // we do not use keys in form messages.welcome
-
-//     interpolation: {
-//       escapeValue: false // react already safes from xss
-//     }
-//   });
-
-// export default i18n;
